b8879819e73878af4291b448714a3708f466e6d9
Merlin <merlin@geeks4change.net>
2021-03-20 02:23:20 +0100

    Robustify

--- tests/src/FunctionalJavascript/ComplexWidgetTest.php
+++ tests/src/FunctionalJavascript/ComplexWidgetTest.php
@@ -958,18 +958,22 @@ class ComplexWidgetTest extends InlineEntityFormTestBase {
         ->setValue($nested_title);
       $assert_session->elementExists('xpath', $double_nested_title_field_xpath)
         ->setValue($double_nested_title);
+      $this->htmlOutput();
 
       // Close all subforms.
       $page->pressButton('Create node 3');
-      $assert_session->waitForButton('Add new node 3');
+      $this->assertNotNull($assert_session->waitForButton('Add new node 3'));
+      $this->htmlOutput();
       $page->pressButton('Create node 2');
-      $assert_session->waitForButton('Add new node 2');
+      $this->assertNotNull($assert_session->waitForButton('Add new node 2'));
+      $this->htmlOutput();
 
       // Re-open all subforms and add a second node 3.
       $page->pressButton('Edit');
-      $assert_session->waitForButton('Update node 2');
+      $this->assertNotNull($assert_session->waitForButton('Update node 2'));
+      $this->htmlOutput();
       $page->pressButton('Add new node 3');
-      $assert_session->waitForButton('Create node 3');
+      $this->assertNotNull($assert_session->waitForButton('Create node 3'));
       $assert_session->elementExists('xpath', $double_nested_title_field_xpath)
         ->setValue("Second $double_nested_title");
       $this->htmlOutput();
@@ -977,8 +981,10 @@ class ComplexWidgetTest extends InlineEntityFormTestBase {
       // Save everything and assert message.
       $page->pressButton('Create node 3');
       $assert_session->waitForButton('Update node 2');
+      $this->htmlOutput();
       $page->pressButton('Update node 2');
-      $assert_session->waitForButton('Add new node 2');
+      $this->assertNotNull($assert_session->waitForButton('Add new node 2'));
+      $this->htmlOutput();
       $page->pressButton('Save');
       $this->htmlOutput();
       $assert_session->pageTextContains("IEF test nested 1 $top_level_title has been created.");
