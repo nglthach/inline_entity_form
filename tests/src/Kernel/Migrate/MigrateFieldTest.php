0bd5cb7e0b609351fb750221d2816f69af563111
alanmoreira <alanmoreira@3716282.no-reply.drupal.org>
2022-07-20 11:41:21 +0300

    Issue #3135178 by Spokje, alanmoreira, Guilherme Rabelo, tauanygb, bruno.bicudo, mrinalini9, Pooja Ganjage, RenatoCostaDev, geek-merlin: Fix Coding Standards.

--- tests/src/Kernel/Migrate/MigrateFieldTest.php
+++ tests/src/Kernel/Migrate/MigrateFieldTest.php
@@ -41,7 +41,7 @@ class MigrateFieldTest extends MigrateTestBase {
    *   The expected cardinality of the field.
    */
   protected function assertEntity($id, $expected_type, $expected_translatable, $expected_cardinality) {
-    list ($expected_entity_type, $expected_name) = explode('.', $id);
+    [$expected_entity_type, $expected_name] = explode('.', $id);
 
     /** @var \Drupal\field\FieldStorageConfigInterface $field */
     $field = FieldStorageConfig::load($id);
